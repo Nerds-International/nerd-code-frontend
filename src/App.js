b97a50e3bce04cd9b3c119a86234167e0d61f168
Demid0 <demid04ka@mail.ru>
2024-10-11 14:10:15 +0300

    testing tests

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 import {observer} from "mobx-react-lite";
+import {useState} from "react";
 
 const App = observer(() => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <MyButton/>
     </div>
   );
 })
 
 export default App;
+
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+  return (
+    <button
+      onClick={handleClick}
+      className="bg-blue-500 hover:bg-blue-700 rounded py-2 px-4 text-white font-bold"
+    >
+      I have been clicked {count} times
+    </button>
+  );
+}
+
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("Example 1 renders successfully", () => {
+  render(<App/>);
+
+  const element = screen.getByText(/I have been clicked \d+ times/i);
+
+  expect(element).toBeInTheDocument();
+})
+

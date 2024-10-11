756f731d4c14b2c66d614e5bdda3e9a5a3308c84
Demid0 <demid04ka@mail.ru>
2024-10-11 13:47:56 +0300

    test commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,15 @@ import './App.css';
 import {observer} from "mobx-react-lite";
 
 const App = observer(() => {
+
+  function aboba() {
+    alert(1 + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      go
+      <button onClick={aboba}>ida</button>
     </div>
   );
 })

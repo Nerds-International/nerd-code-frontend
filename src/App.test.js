9c9077ea949b0c76518d0c217dbdaaa092ccc4f1
AntonSexov <antonvedernikov2003@gmail.com>
2024-10-16 20:39:16 +0300

    G3.5

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import App, { sampleFunction } from './App';
+import App from './App';
 
 test("Example 1 renders successfully", () => {
   render(<App />);

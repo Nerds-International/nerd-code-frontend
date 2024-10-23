e300f1f5dce9339d9f1e09b3d30cfb84dc8f6d7a
anton <antonvedernikov2003@gmail.com>
2024-10-23 07:52:28 +0300

    wip: added testing for battle page

---
+++ src/components/battle/Battle.test.js
@@ -0,0 +1,66 @@
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Battle from "./Battle";
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({ found: false }),
+  }),
+);
+
+describe("Battle Component", () => {
+  beforeEach(() => {
+    fetch.mockClear();
+  });
+
+  test("renders without crashing", () => {
+    render(<Battle />);
+    expect(screen.getByText(/No Match Found/i)).toBeInTheDocument();
+  });
+
+  test("displays loading message when finding a match", async () => {
+    fetch.mockImplementationOnce(() =>
+      Promise.resolve({
+        json: () => Promise.resolve({ found: false }),
+      }),
+    );
+
+    render(<Battle />);
+    fireEvent.click(screen.getByText(/Find Match/i));
+
+    expect(screen.getByText(/Loading.../i)).toBeInTheDocument();
+
+    await waitFor(() =>
+      expect(screen.queryByText(/Loading.../i)).not.toBeInTheDocument(),
+    );
+    expect(screen.getByText(/No Match Found/i)).toBeInTheDocument();
+  });
+  //Тесты ниже мусорные, их надо в дальнейшем поменять
+  test("displays match found message when a match is found", async () => {
+    fetch.mockImplementationOnce(() =>
+      Promise.resolve({
+        json: () => Promise.resolve({ found: true }),
+      }),
+    );
+
+    render(<Battle />);
+    fireEvent.click(screen.getByText(/Find Match/i));
+
+    await waitFor(() =>
+      expect(screen.queryByText(/Loading.../i)).not.toBeInTheDocument(),
+    );
+    expect(screen.getByText(/match found/i)).toBeInTheDocument();
+  });
+
+  test("handles fetch error gracefully", async () => {
+    fetch.mockImplementationOnce(() => Promise.reject("API is down"));
+
+    render(<Battle />);
+    fireEvent.click(screen.getByText(/Find Match/i));
+
+    await waitFor(() =>
+      expect(screen.queryByText(/Loading.../i)).not.toBeInTheDocument(),
+    );
+    expect(screen.getByText(/No Match Found/i)).toBeInTheDocument();
+  });
+});

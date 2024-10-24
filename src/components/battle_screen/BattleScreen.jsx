ac23efd52083e94e37898789368e67d02d8c9acb
anton <antonvedernikov2003@gmail.com>
2024-10-24 16:27:40 +0300

    feat: battlepage base structure and logic

--- src/components/battle_screen/BattleScreen.jsx
+++ src/components/battle_screen/BattleScreen.jsx
@@ -3,7 +3,6 @@ import { observer } from "mobx-react-lite";
 import { useState } from "react";
 import { languageStore } from "../../store/LanguageStore";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { solarizedlight } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 const BattleScreen = observer(() => {
   return (
@@ -18,34 +17,35 @@ const BattleScreen = observer(() => {
 });
 
 export default BattleScreen;
-
-function BattleWindows() {
+//ЭТО ВРЕМЕННАЯ ЗАТЫЧКА
+const BattleWindows = observer(() => {
   const { getCurrentLanguage } = languageStore;
   const [code1, setCode1] = useState("");
   const [code2, setCode2] = useState("");
   return (
     <div>
-      <h1>This is the Test Page!</h1>
-      <h2>Code Block 1</h2>
+      <h2 id="my-code">Code Block 1</h2>
       <textarea
+        aria-labelledby="my-code"
         value={code1}
         onChange={(e) => setCode1(e.target.value)}
         rows={10}
         cols={50}
       />
-      <SyntaxHighlighter language={getCurrentLanguage()} style={solarizedlight}>
+      <SyntaxHighlighter language={getCurrentLanguage()}>
         {code1}
       </SyntaxHighlighter>
-      <h2>Code Block 2</h2>
+      <h2 id="enemy-code">Code Block 2</h2>
       <textarea
+        aria-labelledby="enemy-code"
         value={code2}
         onChange={(e) => setCode2(e.target.value)}
         rows={10}
         cols={50}
       />
-      <SyntaxHighlighter language={getCurrentLanguage()} style={solarizedlight}>
+      <SyntaxHighlighter language={getCurrentLanguage()}>
         {code2}
       </SyntaxHighlighter>
     </div>
   );
-}
+});

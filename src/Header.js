5b3d545ae18523ad321367ffe7b19cc4fd586c9e
svinoczar <alexsupper75@gmail.com>
2024-10-27 16:41:43 +0300

    feat: header

---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import "./Header.css";
+import nerdImage from './img/nerd.png'
+
+export default function Header() {
+    return (
+        <header className="header">
+             <a href="/"> {}
+                <img src={nerdImage} alt="nerd-code" className="nerd-icon" />
+            </a>
+            <nav>
+                <ul className="tabs">
+                    <li className="tab"><a href="/problems">Problems</a></li>
+                    <li className="tab"><a href="/discuss">Discuss</a></li>
+                    <li className="tab"><a href="/battle">Battle</a></li>
+                    <li className="separator"></li> {}
+                    <li className="login"><a href="/login">Log In</a></li>
+                    <li className="signup"><a href="/signup">Sign Up</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}

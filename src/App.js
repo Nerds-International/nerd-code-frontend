a65b0be41ca165d715c4fcd9b86ea78aaa318ad9
svinoczar <alexsupper75@gmail.com>
2024-10-30 16:19:21 +0300

    fix: MainPage routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // App.jsx
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/header/Header';
+import MainPage from './pages/MainPage';
 import BattlePage from './pages/BattlePage';
 import DiscussPage from './pages/DiscussPage';
 import ProblemsPage from './pages/ProblemsListPage';
@@ -13,7 +14,7 @@ const App = () => {
                 <Route path="/problems" element={<ProblemsPage />} />
                 <Route path="/discuss" element={<DiscussPage />} />
                 <Route path="/battle" element={<BattlePage />} />
-                <Route path="/" element={<h1>Home Page</h1>} /> {/* Главная страница */}
+                <Route path="/" element={<MainPage />} />
             </Routes>
         </Router>
     );

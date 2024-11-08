00eab45fafd66ad8966255a7fd84e8422363155d
svinoczar <alexsupper75@gmail.com>
2024-11-08 11:14:24 +0300

    fix: navbar test

--- src/components/navbar/Navbar.test.js
+++ src/components/navbar/Navbar.test.js
@@ -1,10 +1,18 @@
-import {render, screen} from "@testing-library/react";
-import "@testing-library/jest-dom";
-import Navbar from "./Navbar";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './Navbar';
+import '@testing-library/jest-dom/extend-expect';
 
-test("correct rendering", () => {
-  render(<Navbar/>);
-  expect(screen.getByText(/Problems/i)).toBeInTheDocument();
-  expect(screen.getByText(/Discuss/i)).toBeInTheDocument();
-  expect(screen.getByText(/Battle/i)).toBeInTheDocument();
+describe("Navbar", () => {
+  test("correct rendering", () => {
+    render(
+      <Router>
+        <Navbar />
+      </Router>
+    );
+    expect(screen.getByText(/Problems/i)).toBeInTheDocument();
+    expect(screen.getByText(/Discuss/i)).toBeInTheDocument();
+    expect(screen.getByText(/Battle/i)).toBeInTheDocument();
+  });
 });

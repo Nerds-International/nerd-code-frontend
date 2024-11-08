05df54c8bdedd729ec914ad7390d710b5a4dd14b
Demid0 <demid04ka@mail.ru>
2024-11-08 15:48:45 +0300

    lint: imports

--- src/pages/dicuss_page/DiscussPage.test.js
+++ src/pages/dicuss_page/DiscussPage.test.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import {render, screen} from '@testing-library/react';
 import {forumStore} from '../../store/forumStore/ForumStore';
 import DiscussPage from './DiscussPage';

d9250029da5475309db92135aafcd2a22311954f
svinoczar <alexsupper75@gmail.com>
2024-11-08 10:41:12 +0300

    feature: navbar + battle page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,25 +1,28 @@
-import {observer} from "mobx-react-lite";
-import {Menu} from "antd";
-import {pageStore} from "../../store/PageStore";
+import { observer } from "mobx-react-lite";
+import { Menu, Button } from "antd"; // Импортируем Button для кнопок "Log In" и "Sign Up"
+import { pageStore } from "../../store/PageStore";
 import NerdFaceImage from "../nerdFaceImage/NerdFaceImage";
+import { Link } from 'react-router-dom'; // Импортируем Link
+import './Navbar.css'; // Импортируйте ваш CSS файл для Navbar
 
 const Navbar = observer(() => {
-  const {Pages, setCurrentPage} = pageStore;
+  const { Pages, setCurrentPage } = pageStore;
+
   const navbarItems = [
     {
-      label: (<NerdFaceImage/>),
+      label: (<Link to="/"><NerdFaceImage /></Link>), // Добавляем Link к NerdFaceImage
       key: Pages.MAIN,
     },
     {
-      label: 'Problems',
+      label: <Link className="tab" to="/problems">Problems</Link>, // Добавляем Link для Problems
       key: Pages.PROBLEMS_LIST,
     },
     {
-      label: 'Discuss',
+      label: <Link className="tab" to="/discuss">Discuss</Link>, // Добавляем Link для Discuss
       key: Pages.DISCUSS,
     },
     {
-      label: 'Battle',
+      label: <Link className="tab" to="/battle">Battle</Link>, // Добавляем Link для Battle
       key: Pages.BATTLE,
     },
   ];
@@ -28,14 +31,24 @@ const Navbar = observer(() => {
     setCurrentPage(e.key);
   }
 
-  return (<>
-    <Menu
-      onClick={handleClick}
-      mode="horizontal"
-      selectedKeys={[]}
-      items={navbarItems}
-    />
-  </>);
-})
+  return (
+    <div className="header">
+      <Menu
+        className="tabs"
+        mode="horizontal"
+        items={navbarItems}
+        onClick={handleClick}
+        selectedKeys={[]}
+      />
+      <div className="separator" />
+      <div className="login">
+        <Link to="/login">Log In</Link>
+      </div>
+      <Button className="signup" type="primary">
+        <Link to="/signup">Sign Up</Link>
+      </Button>
+    </div>
+  );
+});
 
 export default Navbar;

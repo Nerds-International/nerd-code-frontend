f2280518b5d68badd75048ea80f1b6577473cea6
svinoczar <alexsupper75@gmail.com>
2024-11-13 19:46:51 +0300

    fix: test imports

--- src/components/battle/Battle.test.js
+++ src/components/battle/Battle.test.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { render, fireEvent, waitFor, screen } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import Battle from './Battle';
 
 jest.mock("react-router-dom", () => ({
@@ -15,6 +14,6 @@ describe('Battle Component', () => {
     render(<Battle />);
     fireEvent.change(screen.getByLabelText(/Choose a programming language/i), { target: { value: 'javascript' } });
     fireEvent.click(screen.getByText(/Find Match/i));
-    await waitFor(() => screen.getByText(/The nerd for the battle has been found/i));
+    await screen.findByText(/The nerd for the battle has been found/i);
   });
 });

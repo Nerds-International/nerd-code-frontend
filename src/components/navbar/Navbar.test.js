86e01953038551372cc24491901bcd3555a24ad1
Ilya <isofgh@gmail.com>
2024-11-13 12:06:39 +0300

    fix: little fix

--- src/components/navbar/Navbar.test.js
+++ src/components/navbar/Navbar.test.js
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen} from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './Navbar';
 import '@testing-library/jest-dom/extend-expect';

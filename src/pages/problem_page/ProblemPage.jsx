6862cba28398020d35d2408a500baab32ebca7e5
Ilya <isofgh@gmail.com>
2024-12-14 02:43:13 +0300

    fix: remove href(store works)

--- src/pages/problem_page/ProblemPage.jsx
+++ src/pages/problem_page/ProblemPage.jsx
@@ -1,7 +1,7 @@
 import { observer } from 'mobx-react-lite';
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import ProblemsStore from '../../store/problem/ProblemsStore';
+import problemsStore from "../../store/problem/ProblemsStore";
 import './ProblemPage.css';
 import CodeEditor from "@uiw/react-textarea-code-editor";
 import { languageStore } from "../../store/language/LanguageStore";
@@ -9,7 +9,8 @@ import { languageStore } from "../../store/language/LanguageStore";
 const ProblemPage = observer(() => {
     const [searchParams] = useSearchParams();
     const id = searchParams.get('id');
-    const task = ProblemsStore.tasks.find(task => task.id === id);
+    const task = problemsStore.tasks.find(task => task.id === id);
+    console.log(problemsStore.tasks)
     const [code2, setCode2] = useState("");
     const [code3, setCode3] = useState("");
     const { Languages, getCurrentLanguage, setCurrentLanguage } = languageStore;

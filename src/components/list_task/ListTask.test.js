2f758f4b7ffc62efe6c1548c8bfea24054811331
Ilya <isofgh@gmail.com>
2024-12-14 04:01:29 +0300

    fix: test logic

--- src/components/list_task/ListTask.test.js
+++ src/components/list_task/ListTask.test.js
@@ -1,11 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import ListTask from './ListTask';
-import ProblemsStore from '../../store/problem/ProblemsStore';
+import problemsStore from '../../store/problem/ProblemsStore';
 
 describe('ListTask Component', () => {
     beforeEach(() => {
-        ProblemsStore.tasks = [
+        problemsStore.tasks = [
             { id: 1, name: 'Task 1', description: 'This is the description for task 1', difficulty: 'Easy' },
             { id: 2, name: 'Task 2', description: 'This is the description for task 2', difficulty: 'Medium' },
             { id: 3, name: 'Task 3', description: 'This is the description for task 3', difficulty: 'Hard' },
@@ -13,7 +13,7 @@ describe('ListTask Component', () => {
     });
 
     test('renders ListTask component', () => {
-        render(<ListTask tasks={ProblemsStore.tasks} />);
+        render(<ListTask tasks={problemsStore.tasks} />);
         expect(screen.getByText('Task 1')).toBeInTheDocument();
         expect(screen.getByText('Task 2')).toBeInTheDocument();
         expect(screen.getByText('Task 3')).toBeInTheDocument();

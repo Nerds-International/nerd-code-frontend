fd0f9056bf9ad8fd6e974de08b2961b5a071a3d2
Ilya <isofgh@gmail.com>
2024-12-14 04:11:10 +0300

    fix: test

--- src/components/list_task/ListTask.test.js
+++ src/components/list_task/ListTask.test.js
@@ -5,11 +5,11 @@ import problemsStore from '../../store/problem/ProblemsStore';
 
 describe('ListTask Component', () => {
     beforeEach(() => {
-        problemsStore.tasks = [
+        problemsStore.setTask([
             { id: 1, name: 'Task 1', description: 'This is the description for task 1', difficulty: 'Easy' },
             { id: 2, name: 'Task 2', description: 'This is the description for task 2', difficulty: 'Medium' },
             { id: 3, name: 'Task 3', description: 'This is the description for task 3', difficulty: 'Hard' },
-        ];
+        ]);
     });
 
     test('renders ListTask component', () => {
--- src/store/problem/ProblemsStore.js
+++ src/store/problem/ProblemsStore.js
@@ -2,9 +2,9 @@ import { makeAutoObservable } from 'mobx';
 
 class ProblemsStore {
     tasks = [
-    //    { id: "1", name: 'Task 1', description: 'This is the description for task 1', difficulty: 'Easy', likes: 0, dislikes: 0,  created_at: ""},
-    //    { id: "2", name: 'Task 2', description: 'This is the description for task 2', difficulty: 'Medium', likes: 0, dislikes: 0,  created_at: "" },
-    //    { id: "3", name: 'Task 3', description: 'This is the description for task 3', difficulty: 'Hard', likes: 0, dislikes: 0,  created_at: "" }
+        { id: "1", name: 'Task 1', description: 'This is the description for task 1', difficulty: 'Easy', likes: 0, dislikes: 0,  created_at: ""},
+        { id: "2", name: 'Task 2', description: 'This is the description for task 2', difficulty: 'Medium', likes: 0, dislikes: 0,  created_at: "" },
+        { id: "3", name: 'Task 3', description: 'This is the description for task 3', difficulty: 'Hard', likes: 0, dislikes: 0,  created_at: "" }
     ];
 
     constructor() {

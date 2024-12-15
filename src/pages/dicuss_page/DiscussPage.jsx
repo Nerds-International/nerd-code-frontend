2869408f364e41516a56d9a5f5b97f74ad340304
svinoczar <alexsupper75@gmail.com>
2024-12-15 16:06:12 +0300

    feat: styles for buttons

--- src/pages/dicuss_page/DiscussPage.jsx
+++ src/pages/dicuss_page/DiscussPage.jsx
@@ -1,67 +1,93 @@
 import { observer } from "mobx-react-lite";
 import { forumStore } from "../../store/forum/ForumStore";
 import { useMemo, useState } from "react";
-import "./style.css";
+import "./DiscussPage.css";
 import { Button, Flex, Input, List } from "antd";
 import CreateTopicModal from "../../components/create_topic_modal/CreateTopicModal";
 import TopicCard from "../../components/topic_card/TopicCard";
 
 const DiscussPage = observer(() => {
-  const [isCreateTopicModalVisible, setIsCreateTopicModalVisible] = useState(false)
+  const [isCreateTopicModalVisible, setIsCreateTopicModalVisible] = useState(false);
   const { getTopics } = forumStore;
-  const [filter, setFilter] = useState({})
-  const topics = useMemo(() => getTopics(filter), [filter, getTopics()])
+  const [filter, setFilter] = useState({});
+  const topics = useMemo(() => getTopics(filter), [filter, getTopics]);
 
-  const handleSearch = (e) => { setFilter({ title: e.target.value }) }
+  const handleSearch = (e) => {
+    setFilter({ title: e.target.value });
+  };
   const sortByDateDescending = { time: -1 };
   const sortByLikesDescending = { likes: -1 };
   const sortByMessagesCountDescending = { messages: -1 };
 
-  return (<>
-    <Flex
-      vertical={false}
-      style={{
-        padding: 10,
-        alignItems: 'center',
-        justifyContent: 'space-around'
-      }}
-    >
+  return (
+    <>
       <Flex
         vertical={false}
-        style={{ gap: 50 }}
+        style={{
+          padding: 10,
+          alignItems: "center",
+          justifyContent: "space-around",
+        }}
       >
-        <Button onClick={() => setFilter(sortByDateDescending)}>Dates</Button>
-        <Button onClick={() => setFilter(sortByLikesDescending)}>Likes</Button>
-        <Button onClick={() => setFilter(sortByMessagesCountDescending)}>Hot</Button>
+        <Flex vertical={false} style={{ gap: 50 }}>
+          <Button
+            className="sort-button"
+            onClick={() => setFilter(sortByDateDescending)}
+            color="default"
+            variant="text"
+          >
+            Dates
+          </Button>
+          <Button
+            className="sort-button"
+            onClick={() => setFilter(sortByLikesDescending)}
+            color="default"
+            variant="text"
+          >
+            Likes
+          </Button>
+          <Button
+            className="sort-button"
+            onClick={() => setFilter(sortByMessagesCountDescending)}
+            color="default"
+            variant="text"
+          >
+            Hot
+          </Button>
+        </Flex>
+        <Input className="search" onChange={handleSearch} allowClear style={{ width: "20%", fontFamily: "RopaSans"}} />
       </Flex>
-      <Input onChange={handleSearch} allowClear style={{ width: '20%' }} />
-    </Flex>
-    <List
-      style={{
-        padding: 20,
-        height: 700,
-        overflowY: "scroll",
-        overflowX: "hidden",
-      }}
-      bordered={false}
-      grid={{ column: 2, gutter: 40 }}
-      dataSource={topics}
-      renderItem={(topic) => <List.Item>
-        <TopicCard topic={topic} />
-      </List.Item>}
-    />
-    <Button
-      style={{
-        position: 'absolute',
-        left: '40%',
-        width: '20%',
-      }}
-      onClick={() => setIsCreateTopicModalVisible(true)}
-    >
-      Create topic
-    </Button>
-    <CreateTopicModal visible={isCreateTopicModalVisible} setVisible={setIsCreateTopicModalVisible} />
-  </>);
-})
+      <List
+        style={{
+          marginTop: -10,
+          padding: 20,
+          height: 700,
+          overflowY: "scroll",
+          overflowX: "hidden",
+        }}
+        bordered={false}
+        grid={{ column: 2, gutter: 40 }}
+        dataSource={topics}
+        renderItem={(topic) => (
+          <List.Item style={{ cursor: "pointer" }}>
+            <TopicCard topic={topic} />
+          </List.Item>
+        )}
+      />
+      <Button
+        className="create-topic-button"
+        onClick={() => setIsCreateTopicModalVisible(true)}
+        color="default"
+        variant="text"
+      >
+        Create topic
+      </Button>
+      <CreateTopicModal
+        visible={isCreateTopicModalVisible}
+        setVisible={setIsCreateTopicModalVisible}
+      />
+    </>
+  );
+});
 
 export default DiscussPage;

5f52ea50bc9ffd568d0062a47f982a49dae89f50
svinoczar <alexsupper75@gmail.com>
2024-12-15 14:13:31 +0300

    feat: add loading indicator

--- src/components/battle/Battle.jsx
+++ src/components/battle/Battle.jsx
@@ -1,6 +1,7 @@
 import "./Battle.css";
 import { observer } from "mobx-react-lite";
 import { useState } from "react";
+
 import { useNavigate } from "react-router-dom";
 import { languageStore } from "../../store/language/LanguageStore";
 
@@ -25,14 +26,14 @@ const MatchFinder = observer(() => {
       // const response = await fetch("https://example.com/find-match");
       // const data = await response.json();
 
-      if (getCurrentLanguage() === 'javascript') {
+      if (getCurrentLanguage() === "javascript") {
         setTimeout(() => {
           setMatch(true);
         }, 1000);
         setTimeout(() => {
           setLoading(false);
           navigate("/battle"); // Redirect to BattleScreen with a 2-second delay
-        }, 2000)
+        }, 2000);
       } else {
         setTimeout(() => {
           setLoading(false);
@@ -50,35 +51,35 @@ const MatchFinder = observer(() => {
 
   return (
     <div className="match-finder">
-        <div className="language-selector">
-          <label htmlFor="language-select">Choose a programming language:</label>
-          <select
-            id="language-select"
-            value={getCurrentLanguage()}
-            onChange={(e) => setCurrentLanguage(e.target.value)}
-          >
-            <option value="">--Please choose an option--</option>
-            <option value={Languages.JAVASCRIPT}>JavaScript</option>
-            <option value={Languages.PYTHON}>Python</option>
-          </select>
-          <div className="find-match">
+      <div className="language-selector">
+        <label htmlFor="language-select">Choose a programming language:</label>
+        <select
+          id="language-select"
+          value={getCurrentLanguage()}
+          onChange={(e) => setCurrentLanguage(e.target.value)}
+        >
+          <option value="">--Please choose an option--</option>
+          <option value={Languages.JAVASCRIPT}>JavaScript</option>
+          <option value={Languages.PYTHON}>Python</option>
+        </select>
+        <div className="find-match">
           {!match && !loading && (
-            <button className="find-match-button" onClick={findMatch}>Find Match</button>
+            <button className="find-match-button" onClick={findMatch}>
+              Find Match
+            </button>
           )}
           {!match && loading && (
-            <p className="loading">Looking for nerds...</p>
+            <div className="loading-container">
+              <p className="loading">Looking for nerds...</p>
+              <div className="loader"></div>
+            </div>
           )}
-          {match && loading && (
-            <h2>The nerd for the battle has been found</h2>
-          )}
-          {match!==null && !loading && (
-            <h2>Nerds not found</h2>
-          )}
-          </div>
+          {match && loading && <h2>The nerd for the battle has been found</h2>}
+          {match !== null && !loading && <h2>Nerds not found</h2>}
         </div>
+      </div>
     </div>
   );
 });
-
 
 export default Battle;

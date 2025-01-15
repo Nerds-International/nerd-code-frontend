bd9964c83f7d15dec6d13eb3255751c18fb63916
svinoczar <alexsupper75@gmail.com>
2025-01-15 23:05:17 +0300

    feat: link all discuss endpoints with front

--- src/components/like/Like.jsx
+++ src/components/like/Like.jsx
@@ -2,17 +2,37 @@ import { observer } from "mobx-react-lite";
 import { Flex, Typography } from "antd";
 import { HeartOutlined, HeartFilled } from "@ant-design/icons";
 import { useState } from "react";
-import { authStore } from "../../store/auth/AuthStore"; 
+import { authStore } from "../../store/auth/AuthStore";
+import Cookies from "js-cookie";
 
 const { Title } = Typography;
 
 const Like = observer(({ clickable = false, count, editCount = null, style = { position: "absolute", right: 30, top: 0 } }) => {
   const [liked, setLiked] = useState(false);
 
-  const handleLike = () => {
+  const handleLike = async () => {
     if (editCount) {
-      setLiked(!liked);
-      editCount(liked ? -1 : 1);
+      const newLikedState = !liked;
+      const topicId = window.location.href.split('/')[4];
+      setLiked(newLikedState);
+      editCount(newLikedState ? 1 : -1);
+      try {
+        const endpoint = newLikedState ? `/forums/${topicId}/like` : `/forums/${topicId}/decrlike`;
+        const response = await fetch(`http://localhost:3000${endpoint}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'accessToken': Cookies.get('accessToken'),
+            'id': Cookies.get('id')
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error('Ошибка при отправке лайка/дизлайка');
+        }
+      } catch (error) {
+        console.error("Ошибка при отправке лайка/дизлайка:", error);
+      }
     }
   };
 
